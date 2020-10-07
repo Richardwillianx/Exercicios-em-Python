0b0015207cca8cbefb82f9aa4d769f07fe31cf97
Richardwillianx <Richardwillianx@github.com>
2020-10-07 17:03:20 -0300

    alterações feita no dia 07/10/2020
    Signed-off-by: Richardwillianx <Richardwillianx@github.com>

---
+++ Desafio26.py
@@ -0,0 +1,6 @@
+"""
+Faça um programa que leia uma frase pelo teclado e mostre:
+-> Quantas vezes aparece a letra "a".
+-> Em que posição ela aparece a primeira vez.
+-> Em que posição ela aparece a ultima vez.
+"""
