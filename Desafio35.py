14a679439b952ff798971728237714620c060f6c
Richardwillianx <Richardwillianx@github.com>
2020-10-11 16:19:13 -0300

    alterações feita no dia 11/10/2020
    Signed-off-by: Richardwillianx <Richardwillianx@github.com>

--- Desafio35.py
+++ Desafio35.py
@@ -1,3 +1,15 @@
 """
 Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
 """
+print('-=-'*20)
+print('Analisador de Triângulos')
+print('-=-'*20)
+
+ps = float(input('Primeiro segmento: '))
+ss = float(input('Segundo segmento: '))
+ts = float(input('Terceiro segmento: '))
+print()
+if ps < ss + ts and ss < ps + ts and ts < ps + ss:
+    print('Os segmentos acima FORMAM um triângulo.')
+else:
+    print('Os segmentos acima NÃO FORMAM um triângulo.')
